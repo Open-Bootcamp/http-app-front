9426bb192a2ae05ad0601013decd9e52f0a826e5
Luis Martínez <lemartinezm@unitru.edu.pe>
2022-09-16 18:38:49 -0500

    feat: usage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components/Header/Header';
+import Usage from './components/usage/Usage';
 
 function App () {
   return (
     <div className="App">
       <Header />
+      <Usage />
     </div>
   );
 }

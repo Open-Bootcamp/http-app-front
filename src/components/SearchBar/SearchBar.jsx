0115386b91ad9aca473e91e7777b7f58361e138c
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-09-16 18:36:51 -0500

    Searchbar initialized

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+
+const SearchBar = () => {
+  return (
+        <div>
+            <span>
+                <input type='text' />
+            </span>
+        </div>
+  );
+};
+
+SearchBar.propTypes = {
+
+};
+
+export default SearchBar;

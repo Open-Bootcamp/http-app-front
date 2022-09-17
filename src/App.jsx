8584d7763411ac8539f1dc309e019bae4a6155c7
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-09-17 13:41:21 -0500

    Search bar component filtering results feature on first version

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,35 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import SearchBar from './components/SearchBar/SearchBar';
 
 function App () {
+  const [searchCode, setSearchCode] = useState('');
+
+  const codes = ['100', '101', '200', '202', '301', '404', '500'];
+
+  useEffect(() => {
+    console.log('Do something after counter has changed', searchCode);
+  }, [searchCode]);
+
   const searchHTTPCode = code => {
-    console.log(code);
+    setSearchCode(code);
+    console.log(searchCode);
   };
+
 
   return (
     <div className="App">
       <Header/>
       <SearchBar onSearch={searchHTTPCode}/>
-      {/*TODO: hacer un simulador de las cards y pasar el code como props
+      {/* TODO: hacer un simulador de las cards y pasar el code como props
       */}
+      <div style={{ display: 'flex', flexFlow: 'wrap' }}>
+      {searchCode === ''
+        ? codes.map(el => <h1 key={el} style={{ margin: '0 10px' }}>{el}</h1>)
+        : codes.filter(item => item.includes(searchCode)).map(el => <h1 key={el} style={{ margin: '0 10px' }}>{el}</h1>)
+      }
+      </div>
     </div>
   );
 }

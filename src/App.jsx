f82f1d7e33ccb3d128c7fdad40984459ae96bf82
Santiago Betancur Graciano <santybg_94@hotmail.com>
2022-09-16 22:52:03 -0500

    Searchbar feature intitialized pending testing feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
-import './App.css'
-import Header from './components/Header/Header'
+import './App.css';
+import Header from './components/Header/Header';
+import SearchBar from './components/SearchBar/SearchBar';
 
-function App() {
+function App () {
+  const searchHTTPCode = code => {
+    console.log(code);
+  };
 
   return (
     <div className="App">
       <Header/>
+      <SearchBar onSearch={searchHTTPCode}/>
+      {/*TODO: hacer un simulador de las cards y pasar el code como props
+      */}
     </div>
   );
 }
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,17 +1,26 @@
 import PropTypes from 'prop-types';
+import { BiSearch } from 'react-icons/bi';
+import './searchbar.css';
 
-const SearchBar = () => {
+const SearchBar = ({ onSearch }) => {
   return (
-        <div>
-            <span>
-                <input type='text' />
-            </span>
-        </div>
+    <div className="search-box">
+      <div className="searchbar">
+        <BiSearch className="search-icon" />
+        <input
+          className="searchbar-input"
+          type="text"
+          placeholder="Ingresa el código HTTP"
+          onChange={event => onSearch(event.target.value)}
+        />
+      </div>
+      <button className="search-button">Buscar</button>
+    </div>
   );
 };
 
 SearchBar.propTypes = {
-
+  onSearch: PropTypes.func
 };
 
 export default SearchBar;

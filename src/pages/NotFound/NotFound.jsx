f15ed0401f75cfe24f38aef6297cdc72d64d8880
Erika Vidal <erikavidal@usuarios-iMac.local>
2022-09-20 16:03:58 -0500

    componente 404

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,8 +1,9 @@
+import { Error404 } from "../../components/Error404/Error404";
 
 const NotFound = () => {
   return (
         <div>
-            <h1>Not Found Page</h1>
+            <Error404 />
         </div>
   );
 };
